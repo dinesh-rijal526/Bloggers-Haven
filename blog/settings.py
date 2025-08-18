15648984f7500ebd99bb810b9d877a7365d8b12b
dinesh <rijaldinesh618@gmail.com>
2025-08-18 09:58:07 +0545

    updated settings

--- blog/settings.py
+++ blog/settings.py
@@ -27,10 +27,19 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+if not DEBUG:
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SECURE_SSL_REDIRECT = True
+
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = ['*']
 
 SITE_ID = 4
+
 
 # Application definition
 
