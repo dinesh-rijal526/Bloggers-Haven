0f8b765383d9ab0203912a2413260a1749035ddd
dinesh <rijaldinesh618@gmail.com>
2025-08-18 10:34:51 +0545

    updated setting for debug 2

--- blog/settings.py
+++ blog/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 if not DEBUG:
     SECURE_HSTS_SECONDS = 31536000
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
