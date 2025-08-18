2a073428ded7bd6b7f0df2b6db44136cba0ddf07
dinesh <rijaldinesh618@gmail.com>
2025-08-18 10:45:25 +0545

    updated defult avatar 2

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-def get_default_avatar_path():
-    return 'https://res.cloudinary.com/dhtgmo33l/image/upload/v1755492210/default_dfilos.png'  
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     bio = models.TextField(blank=True, null=True)
@@ -15,9 +12,10 @@ class Profile(models.Model):
     dark_mode = models.BooleanField(default=False)
 
     def save(self, *args, **kwargs):
-        if not self.pk and not self.avatar:
-            self.avatar = get_default_avatar_path()
+        if not self.avatar:
+            self.avatar = 'https://res.cloudinary.com/dhtgmo33l/image/upload/v1755492210/default_dfilos.png'
         super().save(*args, **kwargs)
+
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
